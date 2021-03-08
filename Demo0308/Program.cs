6a6b9807aa80003b4159a14efc9db441ec37c00b
Ting-Yi Lin <ting_yi.lin@365h-brs.de>
2021-03-08 15:30:01 +0800

    Add project files.

---
+++ Demo0308/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo0308
+{
+    /// <summary>
+    ///
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region
+
+            /*
+                        Console.WriteLine("Hello World");
+                        Console.Write("!");
+            */
+
+            //            Console.ReadKey();
+            #endregion
+
+            #region x+y=z
+            /*
+            int x, y; //宣告變數x,y
+            int sum; //宣告變數sum
+            Console.WriteLine("enter a number");
+            x = int.Parse(Console.ReadLine()); //轉型
+
+            Console.WriteLine("enter a number");
+            y = int.Parse(Console.ReadLine());
+
+            sum = x + y;
+
+            Console.WriteLine(sum);
+            Console.WriteLine("The sum of x and y is" + sum + ".");
+            Console.WriteLine($"The sum of x and y is {sum}.");  //作業 字串插值
+            Console.WriteLine($"the sum of x an y is {x + y}");
+
+            Console.ReadLine();
+            */
+            #endregion
+
+            //cw+tab*2 =             Console.WriteLine();
+
+            #region  (x+y)/2=z
+            /*
+            double x, y, z;
+            Console.WriteLine("please enter a number");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter a number");
+            y = double.Parse(Console.ReadLine());
+
+            z = (x + y) / 2;
+            Console.WriteLine(z);
+
+            Console.ReadLine();
+        */
+            #endregion
+
+            #region
+            //(x + y) * (u + v) = z
+            //double x, y, u, v, z;
+            //Console.WriteLine("enter a number");
+            //x = double.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a number");
+            //y = double.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a number");
+            //u = double.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a number");
+            //v = double.Parse(Console.ReadLine());
+            //z = (x + y) * (u + v);
+            //Console.WriteLine(z);
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region a^2+b^2=c
+            //double a, b, c;
+            //Console.WriteLine("enter a");
+            //a = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("enter b");
+            //b = double.Parse(Console.ReadLine());
+
+            //c = Math.Pow(a, 2) + Math.Pow(b, 2);
+            //Console.WriteLine(c);
+
+            //Console.ReadLine();
+            #endregion
+
+            #region x^y=z (y為介於0-1的浮點數)
+
+            //double x, y, z;
+            //Console.WriteLine("enter a number");
+            //x = double.Parse(Console.ReadLine());
+            //Console.WriteLine("enter a float number");
+            //y = double.Parse(Console.ReadLine());
+
+            //z = Math.Pow(x, y);
+            //Console.WriteLine(z);
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region  log(x)=z
+            //double x, z;
+            //Console.WriteLine("enter x");
+            //x = double.Parse(Console.ReadLine());
+            //z = Math.Log(x);
+            //Console.WriteLine(z);
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region y = √𝑥
+            //double y, x;
+            //Console.WriteLine("enter x");
+            //x = double.Parse(Console.ReadLine());
+            //y = Math.Sqrt(x);
+            //Console.WriteLine(y);
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region y = sin(x)
+            //double x, y;
+            //Console.WriteLine("enter x");
+            //x = double.Parse(Console.ReadLine());
+            //y = Math.Sin(x);
+            //Console.WriteLine(y);
+
+            //Console.ReadLine();
+
+            #endregion
+        }
+    }
+}
